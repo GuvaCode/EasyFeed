2bfcd8c265d34ef5261178dc698e9fc18a409e9b
Gunko Vadim <GuvaCode@gmail.com>
2019-08-18 15:01:43 +0500

    Update easyfeed.lps and wayrss.pas

--- src/wayrss.pas
+++ src/wayrss.pas
@@ -15,18 +15,13 @@ uses
 
   { IWayRssInfo }
 
-  IWayRssInfo = interface
-  ['{D7780BF9-33FB-4886-8C40-A0EF5B30165B}']
-  function GetCategory: string;
-  function GetNewsCount: integer;
-  function GetReadCount: integer;
-  function GetURI: string;
-  procedure SetReadCount(AValue: integer);
-  property URI:           string  read GetURI;
- // property Name           string  read GetName;
-  property Category:      string  read GetCategory;
-  property NewsCount:     integer read GetNewsCount;
-  property ReadCount:     integer read GetReadCount write SetReadCount;
+  IWayRssInfo = interface ['{4C46E053-1C4A-430C-A8B3-E3A4DDD46129}']
+  function GetImageUrl:    string;
+  function GetTitle:       string;
+  function GetURL:         string;
+  property URL:            string  read GetURL;
+  property Title:          string  read GetTitle;
+  property ImageUrl:       string  read GetImageUrl;
   end;
 
   type
@@ -35,30 +30,58 @@ uses
 
 implementation
  uses EasyRSS;
-type
+ type
 
   { TWayRssInfo }
-
   TWayRssInfo = class(TInterfacedObject, IWayRssInfo)
   private
-  FURI:String;
-  FCategory:String;
-  FNewsCount:Integer;
-  FReadCount:Integer;
+  FURL:                    string;
+  FTitle:                  string;
+  FImageUrl:               string;
   public
-  constructor Create(URI: String);
-  function GetCategory: string;
-  function GetNewsCount: integer;
-  function GetReadCount: integer;
-  function GetURI: string;
-  procedure SetReadCount(AValue: integer);
+  constructor Create(URL: string);
+  function GetImageUrl:    string;
+  function GetTitle:       string;
+  function GetURL:         string;
+ end;
+
+ { TWayRssInfo }
+
+  constructor TWayRssInfo.Create(URL: string);
+  var
+  Rss: TRSSReader;
+  begin
+   Rss := TRSSReader.Create;
+   try
+    Rss.LoadFromHttp(URL);
+    FURL:=URL;
+    FTitle:=Rss.Title;
+    FImageUrl:=Rss.Image.Url;
+   finally
+    rss.Free;
+   end;
   end;
 
-{ TWayRssInfo }
+  function TWayRssInfo.GetImageUrl: string;
+  begin
+  Result:=FImageUrl;
+  end;
 
+  function TWayRssInfo.GetTitle: string;
+  begin
+  Result:=FTitle;
+  end;
+
+  function TWayRssInfo.GetURL: string;
+  begin
+  Result:=FURL;
+  end;
+
+
+
+{
 constructor TWayRssInfo.Create(URI: String);
 var
-  item: TRSSItem;
   Rss: TRSSReader;
 begin
    rss := TRSSReader.Create;
@@ -67,45 +90,12 @@ begin
     FNewsCount            :=Rss.Count;
     FCategory             :=Rss.Category;
     FURI                  :=URI;
-  //  Rss.Title:=;
-  {  for item in rss.Items do
-    begin
 
-
-
-      WriteLn('<b>Title:</b> ', item.Title);
-      WriteLn('<b>Description:</b> ', item.Description);
-      Write('<hr>');
-    end; }
   finally
     rss.Free;
   end;
-end;
+end;  }
 
-function TWayRssInfo.GetCategory: string;
-begin
-  result:=FCategory;
-end;
-
-function TWayRssInfo.GetNewsCount: integer;
-begin
-  result:=FNewsCount;
-end;
-
-function TWayRssInfo.GetReadCount: integer;
-begin
-  result:=FReadCount;
-end;
-
-function TWayRssInfo.GetURI: string;
-begin
-  result:=FURI;
-end;
-
-procedure TWayRssInfo.SetReadCount(AValue: integer);
-begin
-  //
-end;
 
 end.
 
