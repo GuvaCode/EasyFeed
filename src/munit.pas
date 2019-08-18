907fbed93fac949e6058c582c60b34a0ca7d7836
Gunko Vadim <GuvaCode@gmail.com>
2019-08-18 19:11:21 +0500

    Update easyfeed.lps, munit.lfm, and munit.pas

--- src/munit.pas
+++ src/munit.pas
@@ -17,6 +17,7 @@ type
   { TfrmFeed }
 
   TfrmFeed = class(TForm)
+    ImageStrip: TImageStrip;
     ImgCategory: TImageStrip;
     lstFeed: TDrawList;
     lstCategory: TDrawList;
@@ -46,13 +47,13 @@ implementation
 procedure TfrmFeed.lstCategoryDrawItem(Sender: TObject; Surface: ISurface;
   Index: Integer; Rect: TRectI; State: TDrawState);
 const
-  Margin = -4;
+  Margin = 4;
  var
   Rss: IWayRssInfo;
-  PicRect: TRectI;
+
   S: string;
   I: Integer;
-  D: TRectI;
+  T: TRectF;
   R: TRectF;
   begin
    { If the item is selected, but it a nice gradiant outline and fill }
@@ -60,30 +61,16 @@ const
   { Retreive the current IFileDownload }
     Rss := FWayList[Index];
     S:=Rss.Title;
-  {
-
-  Since TDrawList is a themed control, Theme.Font contains a copy of its IFont }
-  Surface.TextOut(Theme.Font, S, Rect, drCenter);
-  { Draw the photo }
-
-  //PicRect:=FWayPic[Index].ClientRect;
-
-
-  //FWayPic[Index].Surface.CopyTo(Rect, Surface, PicRect);
-
-  // FillRectColor(Surface,D,clBlue);
 
     R := TRectF.Create(32, 32);
-
-    R.X := Rect.X + 4;
-    R.Y := Rect.Y + R.Height/4;
-
-    //Rect.Height - R.Height - 24;
-
+    R.X := Rect.X + Margin;
+    R.Y := Rect.Y + Margin;
     ImgCategory.Draw(Surface, Index, TRectI(R));
 
+    T:=  TRectF.Create(R.X+36,Rect.Y,Rect.Width-R.Width-8,Rect.Height);
+    Surface.TextOut(Theme.Font, S, T, drLeft);
 
-end;
+  end;
 
 procedure TfrmFeed.PictureLoadFromUrl(const APicture: IBitmap;
   const AUrl: String);
@@ -103,7 +90,8 @@ procedure TfrmFeed.PictureLoadFromUrl(const APicture: IBitmap;
 procedure TfrmFeed.FormShow(Sender: TObject);
 begin
   AddRssUrl('https://castle-engine.io/wp/feed/');
-  AddRssUrl('https://castle-engine.io/wp/feed/');
+  AddRssUrl('https://lazplanet.blogspot.com/feeds/posts/default?alt=rss');
+  AddRssUrl('http://www.delphisources.ru/forum/external_mod.php?type=RSS2');
 end;
 
 procedure TfrmFeed.AddRssUrl(RssUrl: string);
@@ -112,9 +100,19 @@ begin
  FWayList.Length:=FWayList.Length+1;
  FWayList.Items[FWayList.Hi]:=AddRss(RssUrl);
  B := NewBitmap;
+ if FWayList.Item[FWayList.Hi].ImageUrl<> '' then
+ begin
  PictureLoadFromUrl(B, FWayList.Item[FWayList.Hi].ImageUrl);
+ B:=B.Resample(32,32);
  ImgCategory.Add(B);
- lstCategory.Count:=FWayList.Length;
+ end
+ else
+ begin
+ ImageStrip.CopyTo(B);
+ B:=B.Resample(32,32);
+ ImgCategory.Add(B);
+ end;
+ LstCategory.Count:=FWayList.Length;
 end;
 
 
